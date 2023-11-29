007485ffdf31aae2a4b1d6cdc3593dad59bdbe90
LLEON <140275669+Leeekode@users.noreply.github.com>
2023-11-28 19:54:40 -0500

    Todo list adding

---
+++ api/models/Todo.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const TodoSchema = new schema({
+  text: {
+    type: String,
+    required: true,
+  },
+
+  complete: {
+    type: Boolean,
+    default: false,
+  },
+  timestamp: {
+    type: String,
+    defaul: Date.now(),
+  },
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports = Todo;
